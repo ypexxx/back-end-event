f802a088574a926659ebd1b435fb6bd8e0642f9f
ypexxx <yua.alvin@gmail.com>
2025-03-12 23:05:56 +0700

    push first time to repository

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import router from "./routes/api";
+
+const app = express();
+const PORT = 3000;
+
+app.use('/api', router);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
